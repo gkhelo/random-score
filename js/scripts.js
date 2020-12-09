f753e09c3fccfa8777764e311337ac8348f411c2
gkhelo <gkhel18@freeuni.edu.ge>
2020-12-09 20:26:58 +0400

    popup div created from js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
 
 function startup() {
+    initializeMenuButtons();
+
     initializeCollapsibleComponents();
     initializeCollapsibleTournamentTables();
+}
+
+function initializeMenuButtons() {
+    initializeMenuButton('search-menu', 'Search Bar');
+    initializeMenuButton('best-teams-menu', 'Best Teams');
+    initializeMenuButton('best-tournaments-menu', 'Best Tournaments');
+}
+
+function initializeMenuButton(menuId, title) {
+    let button = document.getElementById(menuId).children[0];
+    button.addEventListener('click', () => {
+        showPopup(title);
+    });
 }
 
 function initializeCollapsibleComponents() {
@@ -36,4 +51,45 @@ function initializeCollapsibleTournamentTables() {
             target.hidden = expanded;
         });
     });
+}
+
+function showPopup(title) {
+    let wrapper = document.createElement('div');
+    wrapper.id = 'popup-wrapper';
+
+    let popup = document.createElement('div');
+    popup.className = 'popup';
+    
+    popup.appendChild(getPopupHeader(title));
+
+    wrapper.appendChild(popup);
+    document.body.appendChild(wrapper);
+}
+
+function getPopupHeader(title) {
+    let header = document.createElement('div');
+    header.className = 'popup-header';
+ 
+    let titleDiv = document.createElement('div');
+    titleDiv.className = 'popup-title';
+    titleDiv.innerText = title;
+
+    let closeDiv = document.createElement('div');
+    closeDiv.className = 'popup-close';
+    closeDiv.appendChild(getPopupCloseImage());
+
+    header.appendChild(titleDiv);
+    header.appendChild(closeDiv);
+
+    return header;
+}
+
+function getPopupCloseImage() {
+    let img = document.createElement('img');
+    img.src = './images/close.png';
+    img.addEventListener('click', () => {
+        document.body.removeChild(document.getElementById('popup-wrapper'));
+    });
+    
+    return img;
 }
