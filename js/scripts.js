44ae67b099b24fafb7e703766e55443ce7bee990
gkhelo <gkhel18@freeuni.edu.ge>
2020-12-09 17:40:29 +0400

    refactored collapsible component

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 (function() {
-    let collapsibles = document.getElementsByClassName('collapsible');
+    collapsible();
+})()
+
+// Collapsible component
+function collapsible() {
+    let collapsibles = document.getElementsByClassName('component-collapsible');
     
     Array.prototype.forEach.call(collapsibles, c => {
         let target = c.nextElementSibling;
+        let img = c.children[1].children[0];
 
-        c.onclick = () => {
-            let expanded = c.getAttribute('aria-expanded') === 'true';
-            
-            c.setAttribute('aria-expanded', !expanded);
-            target.hidden = expanded;
-        }
+        img.addEventListener('click', function() {
+            if (target.style.display !== 'none') {
+                target.style.display = 'none';
+                img.src = './images/slide-down.png';
+            } else {
+                target.style.display = 'block';
+                img.src = './images/slide-up.png';
+            }
+        });
     });
-})()
+}
